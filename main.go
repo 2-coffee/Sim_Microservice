708bae40b777f8993393434777a1bf478be73306
“kevin” <kevinle1878@gmail.com>
2025-04-28 20:53:31 -0700

    Router handler by Chi package. Made a simple Get request

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger) // To see logs whenever we do any kind of response and request.
+
+	router.Get("/hello", basicHandler) // client asking server to get /hello endpoint
+
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
